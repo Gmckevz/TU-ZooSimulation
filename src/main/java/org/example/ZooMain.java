3df513e0e230f36c6218151dd018a153e1686139
Gmckevz <mckevin.aranda@gcash.com>
2025-07-11 21:58:49 +0800

    Added images

--- src/main/java/org/example/ZooMain.java
+++ src/main/java/org/example/ZooMain.java
@@ -11,6 +11,31 @@ import java.util.Scanner;
 
 public class ZooMain {
 
+    public static void menuScreen(){
+        System.out.println("------Welcome to the Zoo------");
+        System.out.println("What would you like to access?");
+        System.out.println("1. Zoo Admin Console ");
+        System.out.println("2. The Zoo");
+        System.out.println("3. Exit Program");
+        System.out.print("Enter your choice: ");
+    }
+
+    public static void loginScreen(Scanner inputReader,Zoo zooInstance){
+        // add new line
+        System.out.println();
+        System.out.println("=== Welcome to the Zoo Admin Console ===");
+        // add new line
+        System.out.println();
+        System.out.println("Please log in.");
+        System.out.print("Enter username: ");
+        String username = inputReader.next();
+        System.out.print("Enter password: ");
+        String password = inputReader.next();
+        zooInstance.validateAdminUser(username,password);
+    }
+
+
+
     public static void main(String[] args) {
         Scanner inputReader = new Scanner(System.in);
         int choice = 0;
@@ -43,30 +68,14 @@ public class ZooMain {
 
         while(choice != 3) {
             // zoo console, choose if you are accessing as an admin or visitor
-            System.out.println("------Welcome to the Zoo------");
-            System.out.println("What would you like to access?");
-            System.out.println("1. Zoo Admin Console ");
-            System.out.println("2. The Zoo");
-            System.out.println("3. Exit Program");
-            System.out.print("Enter your choice: ");
+            menuScreen();
             choice = inputReader.nextInt();
 
             switch (choice) {
                 case 1:
-                    // add new line
-                    System.out.println();
-                    System.out.println("=== Welcome to the Zoo Admin Console ===");
-                    // add new line
-                    System.out.println();
-                    System.out.println("Please log in.");
-                    System.out.print("Enter username: ");
-                    String username = inputReader.next();
-                    System.out.print("Enter password: ");
-                    String password = inputReader.next();
 
-
+                    loginScreen(inputReader, zooInstance);
                     // validate if username and password is valid
-                    zooInstance.validateAdminUser(username,password);
 
                     if (zooInstance.isAdminUserLoggedIn()) {
                         System.out.println("Login successful. Welcome!");
